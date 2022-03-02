b042fd14a3e253a90ce7a177804616143c1b8c1c
Solcelle <iver.mortensen@gmail.com>
2022-03-02 15:34:15 +0100

    created boid class

--- main.py
+++ main.py
@@ -1,25 +1,72 @@
+from asyncio.windows_events import NULL
 import pygame
-pygame.init
+pygame.init()
 
 # Create screen
-SCREEN_RES = (1280, 720)
-screen = pygame.display.set_mode((SCREEN_RES), 0, 32)
+SCREEN_X = 1024
+SCREEN_Y = 648
+screen = pygame.display.set_mode((SCREEN_X, SCREEN_Y), 0, 32)
 
 GAME_TICK = 60
-CLOCK = pygame.time.Clock()
+clock = pygame.time.Clock()
 
 # Game coloure
 BACKGROUND_COLOR = (20, 22, 30)
+BOID_COLOR = (128, 56, 200)
+
+
+'''Object'''
+class Object():
+	def __init__(self):
+		self.x = 0
+		self.y = 0
+		self.height = 0
+		self.width = 0
+		self.color = (0, 0, 0)
+		self.polygon = [(100,100), (300,100), (150,300)]
+
+'''Move object'''
+class Move_object():
+	def __init__(self):
+		self.speed = 0
+		
+	def move(self, left, right):
+		self.x += -left * time_passed
+		self.x += right * time_passed
+
+'''Draw object'''
+class Draw_object():
+	def draw(self):
+		self.box = pygame.draw.polygon(screen, self.color, self.polygon)
+
+'''Boid'''
+class Boid(Object, Move_object, Draw_object):
+	def __init__(self):
+		super().__init__()
+		self.color = BOID_COLOR
+
+boid1 = Boid()
+
 
 # Game loop
 while True:
 	events = pygame.event.get()
+	for event in events:
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			exit()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE:
+				pygame.quit()
+				exit()
+
 
 	# Sets amount of updates per second
-	time_passed = CLOCK.tick(GAME_TICK) / 1000.0
+	time_passed = clock.tick(GAME_TICK) / 1000.0
 
 	# Set background color
 	screen.fill(BACKGROUND_COLOR)
+	boid1.draw()
 
 	# Update display too show new frame
 	pygame.display.update()
